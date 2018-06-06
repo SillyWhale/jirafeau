263172d74974fa88240edac8dc54b37bb0f471cf
Vincent FRICOU <vincent+github@fricouv.eu>
2018-06-06 14:24:33 +0200

    Add php7-session package

--- includes/init.sh
+++ includes/init.sh
@@ -4,4 +4,4 @@ chown -R nobody:nogroup ${JF_DATA_DIR}
 cd ${JF_ROOT_DIR}
 supervisord -c /usr/local/etc/supervisord.conf &
 sleep 5
-tail -f /supervisord.log /var/log/nginx/jirafeau_access.log /var/log/nginx/jirafeau_error.log /var/log/nginx/nextcloud.access.log
+tail -f /supervisord.log /var/log/nginx/jirafeau_access.log /var/log/nginx/jirafeau_error.log /var/log/nginx/jirafeau.access.log
