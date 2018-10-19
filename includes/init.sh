a41d7074675cd6d3aa733df3e737ac65c9988aa4
Vincent FRICOU <vincent+gitkraken@fricouv.eu>
2018-10-19 17:02:54 +0200

    enc: update files to prevent php timeout. Adding capability to change file cipher methodÂ

--- includes/init.sh
+++ includes/init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-SIZE_LIMIT=${SIZE_LIMIT:-'1G'}
+SIZE_LIMIT=${SIZE_LIMIT:-'100M'}
 PURGE_ENABLED=${PURGE_ENABLED:-'false'}
+JF_CIPHER=${JF_CIPHER:-''}
 
 sed -i "s/client_max_body_size 1G/client_max_body_size ${SIZE_LIMIT}/g" /etc/nginx/conf.d/default.conf
 
@@ -10,12 +11,19 @@ then
     echo '00 00 * * * php /Jirafeau/admin.php clean_async' >> /etc/crontabs/www-data
 fi
 
-chown -R nobody:nogroup ${JF_ROOT_DIR}
-chown -R nobody:nogroup ${JF_DATA_DIR}
-chmod -R g+w,o+rwx ${JF_DATA_DIR}
+if [ "${JF_CIPHER}" != '' ] 
+then 
+    sed -i "s/rijndael-256/${JF_CIPHER}/g" /Jirafeau/lib/functions.php
+    sed -i "s/rijndael-256/${JF_CIPHER}/g" /Jirafeau/f.php
+fi
+
 mkdir ${JF_DATA_DIR}/files
 mkdir ${JF_DATA_DIR}/links
 mkdir ${JF_DATA_DIR}/async
+chown -R nobody:nogroup ${JF_ROOT_DIR}
+chown -R nobody:nogroup ${JF_DATA_DIR}
+find ${JF_DATA_DIR} -type f -exec chmod g+w,o+rw {} \;
+find ${JF_DATA_DIR} -type d -exec chmod g+w,o+rwx {} \;
 cd ${JF_ROOT_DIR}
 supervisord -c /usr/local/etc/supervisord.conf &
 sleep 5
