d29d0883356affc9aa6a01c6d0f82698236236e1
Vincent FRICOU <vincent@fricouv.eu>
2019-05-24 10:54:39 +0200

    fix: Update cron tasks to be launched by root. Update to no duplicate cron entries

--- includes/init.sh
+++ includes/init.sh
@@ -5,14 +5,14 @@ JF_CIPHER=${JF_CIPHER:-''}
 
 sed -i "s/client_max_body_size 1G/client_max_body_size ${SIZE_LIMIT}/g" /etc/nginx/conf.d/default.conf
 
-if [ "${PURGE_ENABLED}" == 'true' ]
+if [ "${PURGE_ENABLED}" == 'true' && ! -f "/runned_once" ]
 then
-    echo '00 00 * * * php /Jirafeau/admin.php clean_expired' >> /etc/crontabs/www-data
-    echo '00 00 * * * php /Jirafeau/admin.php clean_async' >> /etc/crontabs/www-data
+    echo '00 00 * * * php /Jirafeau/admin.php clean_expired' >> /etc/crontabs/root
+    echo '00 00 * * * php /Jirafeau/admin.php clean_async' >> /etc/crontabs/root
 fi
 
-if [ "${JF_CIPHER}" != '' ] 
-then 
+if [ "${JF_CIPHER}" != '' ]
+then
     sed -i "s/rijndael-256/${JF_CIPHER}/g" /Jirafeau/lib/functions.php
     sed -i "s/rijndael-256/${JF_CIPHER}/g" /Jirafeau/f.php
 fi
@@ -26,5 +26,6 @@ find ${JF_DATA_DIR} -type f -exec chmod g+w,o+rw {} \;
 find ${JF_DATA_DIR} -type d -exec chmod g+w,o+rwx {} \;
 cd ${JF_ROOT_DIR}
 supervisord -c /usr/local/etc/supervisord.conf &
+touch /runned_once
 sleep 5
 tail -f /supervisord.log /var/log/nginx/jirafeau_access.log /var/log/nginx/jirafeau_error.log /var/log/nginx/jirafeau.access.log
